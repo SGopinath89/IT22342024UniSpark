5c7df79f34a24569bdfb6e1141a5e1fc89d9ba45
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-06-30 22:26:37 +0530

    modify chatRoute

--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -6,5 +6,52 @@ const{default:mongoose}=require('mongoose')
 //const { verifyToken } = require("../security/auth");
 const name="Chat";
 
+router.get("/", verifyToken, (req, res) => {
+    Service.getAll(res,Chat,name).catch((error)=>{
+        res.status(500).send("Server Error")
+    })
+});
+router.get("/:id", verifyToken, (req,res)=>{
+    Service.getBYId(req,res,Chat,name).catch((error)=>{
+        res.status(500).send("Server error")
+    })
+});
+
+router.post("/", verifyToken, (req, res) => {
+    const { Message } = req.body;
+    if (!Message) {
+    res.status(404).send("Please provide required fields");
+    } else {
+        Service.add(res,Chat,{Message}).catch((error)=>{
+            res.status(500).send("Server  error")
+        })
+    } 
+});
+
+router.delete("/:id",verifyToken,(req,res)=>{
+    Service.deleteById(req,res,Chat,name).catch((error)=>{
+        res.status(500).send(error+"Server Error")
+    })
+});
+
+router.put("/:id", verifyToken, async(req, res) => {
+    const id = req.params.id;
+    const chats = await Chat.findById(id).catch((error) => {
+    console.error(error);
+    });
+    if (!chats) {
+    res.status(404).send("Author Not found");
+    } else {
+    const {Message} = req.body;
+    if (!Message) {
+        res.status(400).send("Please provide required fields");
+    } else {
+        Service.update(res,chats,{Message}).catch((error)=>{
+            res.status(500).send(error+"Server Error")
+           })
+        
+    }
+    }
+});
 
 module.exports = router;
