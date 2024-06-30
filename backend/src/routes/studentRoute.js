8238e3943c1a6209a5cfe0d5c8090f9164b0e5bd
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-06-30 20:51:57 +0530

    modify studentRoute

--- backend/src/routes/studentRoute.js
+++ backend/src/routes/studentRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Lecturer= require("../models/Student");
+const Student= require("../models/Student");
 const Service = require("../service/GenericService")
 const{default:mongoose}=require('mongoose')
 const name="Student";
