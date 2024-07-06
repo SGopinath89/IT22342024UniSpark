6e6012497edfe4713e5c6acfecc31ffce621271a
PoorwaSandulmini <139644198+PoorwaSandulmini@users.noreply.github.com>
2024-07-07 00:57:50 +0530

    Update Subject.js

--- backend/src/models/Subject.js
+++ backend/src/models/Subject.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema
 const SubjectSchema = new Schema({
     SubjectCode: { type :String,required:true},
 	Subjectname: { type :String,required:true},
-	Course_ID: { type: mongoose.Types.ObjectId, required: true },
-
+	Course_ID: { type: Schema.Types.ObjectId,required:true, ref: 'courses' }
 })
+
 
 const Subject= mongoose.model('subject',SubjectSchema)
 module.exports=Subject
