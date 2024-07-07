95f545bf29071fa8c844be02afd1bcfd12b36c89
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-07-07 16:51:28 +0530

    fix:index

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const courseRoute=require('./src/routes/courseRoute')
 const subjectRoute=require('./src/routes/subjectRoute')
 const resourceRoute=require('./src/routes/resourceRoute')
 
-app/use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 const bodyparser=require('body-parser')
 
@@ -29,13 +29,13 @@ mongoose.connect('mongodb://localhost:27017/UniSparkDb').then(()=>{
 
 app.use('/admin',adminRoute)
 app.use('/instructor',instructorRoute)
-app.use('./lecturer',lecturerRoute)
+app.use('/lecturer',lecturerRoute)
 app.use('/student',studentRoute)
 
 app.use('/chat',chatRoute)
 app.use('/course',courseRoute)
 app.use('/subject',subjectRoute)
-app.use('resource',resourceRoute)
+app.use('/resource',resourceRoute)
 
 
 app.listen(port,()=>{
